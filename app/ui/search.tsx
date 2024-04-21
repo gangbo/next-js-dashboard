83beb71a41d8f2dc9313b2696fb7babb8a6e9c19
gangbo.dai <daigangbo@gmail.com>
2024-04-21 15:43:29 +0800

    #

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,14 +1,15 @@
 'use client';
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import {MagnifyingGlassIcon} from '@heroicons/react/24/outline';
+import {useSearchParams, usePathname, useRouter} from 'next/navigation';
+import {useDebouncedCallback} from 'use-debounce';
 
-export default function Search({ placeholder }: { placeholder: string }) {
-    const searchParams  = useSearchParams();
+export default function Search({placeholder}: { placeholder: string }) {
+    const searchParams = useSearchParams();
     const pathname = usePathname();
-    const { replace } = useRouter();
+    const {replace} = useRouter();
     console.log(searchParams);
-    function handleSearch(term: string) {
+    const handleSearch = useDebouncedCallback((term: string) => {
         console.log(term);
         const params = new URLSearchParams(searchParams);
         if (term) {
@@ -17,19 +18,20 @@ export default function Search({ placeholder }: { placeholder: string }) {
             params.delete('query');
         }
         replace(`${pathname}?${params.toString()}`);
-    }
-  return (
-    <div className="relative flex flex-1 flex-shrink-0">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
-      <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
-        onChange={(e) => handleSearch(e.target.value)}
-        defaultValue={searchParams.get('query')?.toString()}
-      />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-    </div>
-  );
+    }, 2000)
+    return (
+        <div className="relative flex flex-1 flex-shrink-0">
+            <label htmlFor="search" className="sr-only">
+                Search
+            </label>
+            <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                placeholder={placeholder}
+                onChange={(e) => handleSearch(e.target.value)}
+                defaultValue={searchParams.get('query')?.toString()}
+            />
+            <MagnifyingGlassIcon
+                className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
+        </div>
+    );
 }
